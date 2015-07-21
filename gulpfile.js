604d73517b8715436880a87868640e4902ef84a0
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-07-21 04:36:46 +0300

    add gulp file

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+
+gulp.task('sass', function () {
+  gulp.src('./front/styles/**/*.scss')
+    .pipe(concat('style.scss'))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./front/static/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./front/styles/**/*.scss', ['sass']);
+});
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
