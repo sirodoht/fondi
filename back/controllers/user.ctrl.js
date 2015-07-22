bfdedb9d3504f6a3018bb3fa5e6b110a99dfe468
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-07-22 04:31:21 +0300

    add user ctrl
    implement default method which fetches gravatar :D

---
+++ back/controllers/user.ctrl.js
@@ -0,0 +1,16 @@
+var gravatar = require('gravatar');
+
+module.exports = userCtrl = {};
+
+userCtrl.default = function(req, res, next) {
+  var user = req.params.user;
+
+  var userEmail = user + '@gmail.com';
+  var gravatarImgUrl = gravatar.url(userEmail, {s: '100', r: 'x', d: 'retro'}, false);
+
+  res.render('user', {
+    user: user,
+    userImg: gravatarImgUrl,
+  });
+  next();
+};
