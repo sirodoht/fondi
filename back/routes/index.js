6a619ce2de071a42707ec123d0b06c9b99ed0fe8
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-08-06 02:40:29 +0300

    add user register routes

--- back/routes/index.js
+++ back/routes/index.js
@@ -7,6 +7,10 @@ var userCtrl = require('../controllers/user.ctrl');
 /* GET home page. */
 router.get('/', homeCtrl.default);
 
+/* User register */
+router.get('/join', userCtrl.getRegister);
+router.post('/register', userCtrl.register);
+
 /* GET user page. */
 router.get('/:user', userCtrl.default);
 
