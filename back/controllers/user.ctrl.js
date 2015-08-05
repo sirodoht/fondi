33e3b9a0267b96f8bc93c142bbee0f745836cdb8
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-08-06 02:38:14 +0300

    add function to post to register new user

--- back/controllers/user.ctrl.js
+++ back/controllers/user.ctrl.js
@@ -18,3 +18,18 @@ userCtrl.default = function(req, res, next) {
 userCtrl.getRegister = function (req, res, next) {
   res.render('register');
 };
+
+userCtrl.register = function (req, res, next) {
+  console.log(req.body);
+  models.User.create({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }).then(function() {
+    res.redirect('/' + req.username);
+  });
+
+  // now instantiate an object
+  // var task = Task.build({title: 'very important task'})
+};
