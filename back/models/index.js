08fd59ee23f0188ef0d39ab0f38dc0daedfaaae1
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-08-23 01:28:19 +0300

    get back to node env var

--- back/models/index.js
+++ back/models/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var Sequelize = require('sequelize');
 
-var env = process.env.BUILD_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
