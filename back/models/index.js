7de3f95ec6d8a0cd68a9c60b60852097f0b8ad3d
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-08-23 00:48:07 +0300

    use explicit production var for config

--- back/models/index.js
+++ back/models/index.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var Sequelize = require('sequelize');
 
-var env = process.env.NODE_ENV || 'development';
+var env = 'production';
 var config = require(__dirname + '/../../config/config.json')[env];
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 
