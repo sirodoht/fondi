3d146f3ec2f05229d4e319626bf5e91a5cfc782d
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-08-31 04:36:12 +0300

    restore gulp sass
    Revert gulp style renaming

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
-gulp.task('styles', function () {
+gulp.task('sass', function () {
   gulp.src('./front/styles/**/*.scss')
     .pipe(concat('style.scss'))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./front/static/css'));
 });
 
-gulp.task('styles:watch', function () {
-  gulp.watch('./front/styles/**/*.scss', ['styles']);
+gulp.task('sass:watch', function () {
+  gulp.watch('./front/styles/**/*.scss', ['sass']);
 });
 
 gulp.task('default', function () {
   console.log('# Gulp tasks');
-  console.log('gulp styles :: Compiles Sass styles');
+  console.log('gulp sass :: Compiles Sass styles');
 });
