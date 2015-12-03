33d1bcf03f8472d501ca5ce747efbdbb6b907833
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-12-03 04:08:44 +0200

    change anonymous functions define styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   gulp.src('./front/styles/**/*.scss')
     .pipe(concat('style.scss'))
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('./front/static/css'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', function() {
   gulp.watch('./front/styles/**/*.scss', ['sass']);
 });
 
-gulp.task('default', function () {
+gulp.task('default', function() {
   console.log('# Gulp tasks');
   console.log('gulp sass :: Compiles Sass styles');
 });
--- test/index.js
+++ test/index.js
@@ -5,9 +5,9 @@ var chai = require('chai');
 var expect = chai.expect;
 var req = require('request');
 
-describe('Homepage', function () {
-  it('should get a 200 on the home page', function (done) {
-    req.get('http://localhost:3000/', function (err, res, body) {
+describe('Homepage', function() {
+  it('should get a 200 on the home page', function(done) {
+    req.get('http://localhost:3000/', function(err, res, body) {
       expect(res.statusCode).to.equal(200);
       done();
     });
