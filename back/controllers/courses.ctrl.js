0f0b37f02743ea57875cecb87582f4ef3fbed9af
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-12-07 00:50:00 +0200

    refactor courses ctrl

--- back/controllers/courses.ctrl.js
+++ back/controllers/courses.ctrl.js
@@ -1,27 +1,30 @@
-var gravatar = require('gravatar');
-var passport = require('passport');
+// var gravatar = require('gravatar');
+// var passport = require('passport');
 
 var models = require('../models');
 
-module.exports = coursesCtrl = {};
+var coursesCtrl = module.exports = {};
 
-coursesCtrl.list = function(req, res, next) {
+coursesCtrl.list = function(req, res) {
   models.Course.findAll()
     .then(function(allCourses) {
-      console.log('allCourses', allCourses);
+      // console.log('allCourses', allCourses);
       // res.render('user-courses');
       res.send(allCourses);
     });
 };
 
-coursesCtrl.getCreate = function(req, res, next) {
+coursesCtrl.getCreate = function(req, res) {
   res.render('course-create');
 };
 
-coursesCtrl.create = function(req, res, next) {
+coursesCtrl.create = function(req, res) {
+  var user = req.user;
+
   models.Course.create(req.body)
-    .then(function() {
-      res.redirect('/');
+    .then(function (course) {
+      user.addCourse(course);
+      res.redirect('/courses');
     });
 
 };
