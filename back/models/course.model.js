c708987426ea5d0c9c476e08578bf448d69cfe91
Theodore Keloglou <theodorekeloglou@gmail.com>
2015-12-07 00:50:31 +0200

    refactor course model

--- back/models/course.model.js
+++ back/models/course.model.js
@@ -1,26 +1,12 @@
-// var Promise = require('bluebird');
 // var Sequelize = require('sequelize');
 
 module.exports = function(sequelize, DataTypes) {
   var attributes = {
     name: DataTypes.STRING,
-    owner: DataTypes.STRING,
-    desc: DataTypes.STRING,
+    description: DataTypes.TEXT,
   };
 
-  var options = {};
-  options.classMethods = {
-    // associate: function(models) {
-    //   Course.belongsTo(models.User, {
-    //     onDelete: 'CASCADE',
-    //     foreignKey: {
-    //       allowNull: true
-    //     }
-    //   });
-    // }
-  };
-
-  var Course = sequelize.define('Course', attributes, options);
+  var Course = sequelize.define('Course', attributes);
 
   return Course;
 };
