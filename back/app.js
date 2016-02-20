e5b5b3b678e4bddd373fc163c3db44e580adfbd3
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-02-21 01:05:53 +0200

    move app.js to back and start server from there

--- back/app.js
+++ back/app.js
@@ -8,24 +8,26 @@ var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-var routes = require('./back/routes/index');
-var db = require('./back/models/index');
-var models = require('./back/models');
+var routes = require('./routes/index');
+var helpers = require('./util/helpers');
+var listeners = require('./util/listeners');
+var db = require('./models/index');
+var models = require('./models');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'front/views'));
+app.set('views', path.join(__dirname, '../front/views'));
 app.set('view engine', 'jade');
 
 // Enable CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
-// app.use(favicon(path.join(__dirname, 'front/static', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, '../front/static', 'favicon.ico')));
 app.use(logger('dev'));
 
 app.use(bodyParser.json());
@@ -46,12 +48,12 @@ passport.use(new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
     // session: false,
-  }, function(username, password, done) {
+  }, function (username, password, done) {
     models.User.findOne({
       where: {
         username: username
       }
-    }).then(function(user) {
+    }).then(function (user) {
       console.log('did this run 1');
       // if (err) {
       //   console.log('error 1', err);
@@ -74,42 +76,39 @@ passport.use(new LocalStrategy({
   }
 ));
 
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user.username);
 });
 
-passport.deserializeUser(function(username, done) {
+passport.deserializeUser(function (username, done) {
   models.User.findOne({
     where: {
       username: username
     }
-  }).then(function(user) {
+  }).then(function (user) {
     done(null, user);
   });
 
-  // User.findById(id, function(err, user) {
+  // User.findById(id, function (err, user) {
   //   done(err, user);
   // });
 });
 
-app.use(express.static(path.join(__dirname, 'front/static')));
+app.use(express.static(path.join(__dirname, '../front/static')));
 
 app.use('/', routes);
 // app.use('/:user', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// development error handler, will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -118,9 +117,8 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+// production error handler, no stacktraces leaked to user
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -128,5 +126,15 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var port = helpers.normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+models.sequelize.sync()
+  .then(function () {
+    app.listen(port);
+    app.on('error', listeners.onError);
+    app.on('listening', listeners.onListening);
+    console.log('Server running on port ' + port);
+  });
 
 module.exports = app;
