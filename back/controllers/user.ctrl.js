49958d3e50f1b8022ce5e1775dbffbe50c06605f
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-02-21 02:53:06 +0200

    cleanup user ctrl

--- back/controllers/user.ctrl.js
+++ back/controllers/user.ctrl.js
@@ -1,56 +1,43 @@
-var gravatar = require('gravatar');
-var passport = require('passport');
-
 var models = require('../models');
 
-module.exports = userCtrl = {};
+var userCtrl = module.exports = {};
 
-userCtrl.default = function(req, res, next) {
+userCtrl.getUser = function (req, res) {
   var userParam = req.params.user;
 
-  // retrieve user from db model
   models.User.findOne({
     where: {
       username: userParam,
-    }
-  }).then(function(user) {
-    // populate variables to sent to user view
-
+    },
+  }).then(function (user) {
     var userEmail = '';
     if (user !== null) {
       userEmail = user.dataValues.email;
     }
 
-    var gravatarImgUrl = gravatar.url(userEmail, {s: '260', r: 'x', d: 'retro'}, false);
-
-    // var fullName = user.getFullname();
-
     res.render('user', {
       email: userEmail,
-      img: gravatarImgUrl,
       name: user.name,
       username: user.username,
     });
   });
-
-  // next();
 };
 
-userCtrl.getRegister = function(req, res, next) {
+userCtrl.getRegister = function (req, res) {
   res.render('register');
 };
 
-userCtrl.register = function(req, res, next) {
+userCtrl.register = function (req, res) {
   models.User.create({
     username: req.body.username,
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
-  }).then(function() {
+  }).then(function () {
     res.redirect('/' + req.body.username);
   });
 };
 
-userCtrl.getLogin = function(req, res, next) {
+userCtrl.getLogin = function (req, res) {
   res.render('login');
 };
