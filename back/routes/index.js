d1e9aa5eb6a4398fa9a61d807e7602ec811cb0a7
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-02-21 02:27:43 +0200

    refactor router

--- back/routes/index.js
+++ back/routes/index.js
@@ -1,31 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 var indexCtrl = require('../controllers/index.ctrl');
 var userCtrl = require('../controllers/user.ctrl');
 var coursesCtrl = require('../controllers/courses.ctrl');
 
-// Home page
-router.get('/', indexCtrl.default);
-
-// User register
-router.get('/join', userCtrl.getRegister);
+/* API routes */
 router.post('/register', userCtrl.register);
-
-// User login
-router.get('/login', userCtrl.getLogin);
-router.post('/login', userCtrl.login);
+router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 
 router.get('/courses', coursesCtrl.list);
-router.get('/courses/new', coursesCtrl.getCreate);
 router.post('/courses', coursesCtrl.create);
 
-// User page
-router.get('/:user', userCtrl.default);
+/* Frontend routes */
+router.get('/', indexCtrl.getIndex);
 
-// User courses
-// router.get('/:user/courses/create', coursesCtrl.create);
+router.get('/join', userCtrl.getRegister);
+router.get('/login', userCtrl.getLogin);
 
-router.get('/following', userCtrl.default);
+router.get('/courses/new', coursesCtrl.getCreate);
+router.get('/courses/new/github', coursesCtrl.getCreateGithub);
+
+router.get('/:user', userCtrl.getUser);
+// router.get('/:user/courses', coursesCtrl.create);
+
+router.get('/following', userCtrl.getUser);
 
 module.exports = router;
