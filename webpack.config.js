245ea49c745e7fff884331be39ca34b2537c2330
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-03-08 01:23:14 +0200

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'front/scripts/app.js'),
+  output: {
+    path: path.resolve(__dirname, 'front/static/js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
