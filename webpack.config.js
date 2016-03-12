1756d981b2b82cfb57011f95232f542303577e0e
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-03-12 22:52:41 +0200

    change frontapp boot file on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'front/scripts/app.js'),
+  entry: path.resolve(__dirname, 'front/scripts/index.js'),
   output: {
     path: path.resolve(__dirname, 'front/static/js'),
     filename: 'bundle.js',
