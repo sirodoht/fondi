6b89f72b3cbeee2a623253060bd59e38509af7fc
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-03-12 22:49:05 +0200

    add new routes for courses

--- back/routes/index.js
+++ back/routes/index.js
@@ -19,10 +19,11 @@ router.post('/register', userCtrl.register);
 router.get('/login', userCtrl.getLogin);
 router.post('/login', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login' }));
 
+router.get('/profile', userCtrl.getUser);
 router.get('/new', coursesCtrl.getCreate);
+router.get('/:username', coursesCtrl.getOwnCourses);
 router.get('/:username/:courseId', coursesCtrl.getCourse);
+router.get('/:username/:courseId/new', coursesCtrl.getNewSection);
 router.get('/:username/:courseId/:sectionId', coursesCtrl.getSection);
-
-router.get('/:username', userCtrl.getUser);
 
 module.exports = router;
