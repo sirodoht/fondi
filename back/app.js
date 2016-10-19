b645b6a48776b17c1f10a714f1305add68d618d0
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-10-19 21:02:46 +0300

    logging

--- back/app.js
+++ back/app.js
@@ -38,7 +38,7 @@ app.use(function (req, res, next) {
 });
 
 // app.use(favicon(path.join(__dirname, '../front/static', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev')); // FIXME
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -122,7 +122,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler, no stacktraces leaked to user
+// TODO: production error handler, no stacktraces leaked to user
 app.use(function (err, req, res) {
   res.status(err.status || 500);
   res.render('error', {
