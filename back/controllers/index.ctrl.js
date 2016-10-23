71335d2ddb1751ce5e61935b6511fc519754aebe
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-10-24 02:55:20 +0300

    update index ctrl

--- back/controllers/index.ctrl.js
+++ back/controllers/index.ctrl.js
@@ -1,5 +1,40 @@
+var models = require('../models');
+
 var indexCtrl = module.exports = {};
 
 indexCtrl.getIndex = function (req, res) {
-  res.render('index');
+  let user = null;
+  let userCourses = null;
+
+  if (req.user) {
+    models.User.findOne({
+      where: {
+        id: req.user.id,
+      },
+    })
+      .then(function (resUser) {
+        user = resUser;
+        return resUser.getCourses({raw: true})
+      })
+      .then(function (resCourses) {
+        userCourses = resCourses;
+        return models.Course.findAll();
+      })
+      .then(function (resCourse) {
+        res.render('index', {
+          username: user.username,
+          name: user.name,
+          bio: user.bio,
+          courses: userCourses,
+          publicCourses: resCourse,
+        });
+      });
+  } else {
+    models.Course.findAll()
+      .then(function (courses) {
+        res.render('index', {
+          publicCourses: courses,
+        });
+      });
+  }
 };
