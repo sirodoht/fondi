cdb92a5fca51bc47a8da07a3ec81411caab0abae
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-10-24 03:03:48 +0300

    refactor promise chains on courses ctrl

--- back/controllers/courses.ctrl.js
+++ back/controllers/courses.ctrl.js
@@ -35,72 +35,84 @@ coursesCtrl.getCreate = function (req, res) {
 };
 
 coursesCtrl.create = function (req, res) {
-  var courseDetails = {
+  let resCourse = null;
+
+  const courseDetails = {
     name: req.body.name,
     description: req.body.desc,
   };
   models.Course.create(courseDetails)
-    .then(function (course) {
-      models.User.findOne({where: {id: req.user.id}})
-        .then(function (user) {
-          user.addCourse(course);
-          res.redirect('/' + req.user.username + '/' + course.id);
-        });
+    .then(function (resCourse) {
+      newCourse = resCourse;
+      return models.User.findOne({where: {id: req.user.id}})
+    })
+    .then(function (user) {
+      user.addCourse(newCourse);
+      res.redirect('/' + req.user.username + '/' + newCourse.id);
     });
 };
 
 coursesCtrl.getCourse = function (req, res) {
+  let course = null;
+
   models.Course.findOne({where: {id: req.params.courseId}})
-    .then(function (course) {
-      course.getSections({raw: true})
-        .then(function (sections) {
-          res.render('courses/single', {
-            username: req.params.username,
-            courseId: course.id,
-            name: course.name,
-            description: course.description,
-            sections: sections,
-          });
-        });
+    .then(function (resCourse) {
+      course = resCourse;
+      return resCourse.getSections({raw: true});
+    })
+    .then(function (sections) {
+      res.render('courses/single', {
+        username: req.params.username,
+        courseId: course.id,
+        name: course.name,
+        description: course.description,
+        sections: sections,
+      });
     });
 };
 
 coursesCtrl.getEditCourse = function (req, res) {
+  let course = null;
+
   models.Course.findOne({where: {id: req.params.courseId}})
-    .then(function (course) {
-      course.getSections({raw: true})
-        .then(function (sections) {
-          res.render('courses/edit', {
-            username: req.user.username,
-            courseId: course.id,
-            name: course.name,
-            desc: course.description,
-            sections: sections,
-          });
-        });
+    .then(function (resCourse) {
+      course = resCourse;
+      return resCourse.getSections({raw: true})
+    })
+    .then(function (sections) {
+      res.render('courses/edit', {
+        username: req.user.username,
+        courseId: course.id,
+        name: course.name,
+        desc: course.description,
+        sections: sections,
+      });
     });
 };
 
 coursesCtrl.getOwnCourses = function (req, res) {
-  models.User.findOne({
+  let user = null;
+
+  return models.User.findOne({
     where: {
       id: req.user.id,
     },
   })
-    .then(function (user) {
-      user.getCourses({raw: true})
-        .then(function (courses) {
-          res.render('user-courses', {
-            username: user.username,
-            bio: user.bio,
-            courses: courses,
-          });
-        })
-    });
+    .then(function (resUser) {
+      user = resUser;
+      return user.getCourses({raw: true})
+    })
+    .then(function (courses) {
+      res.render('user-courses', {
+        username: user.username,
+        bio: user.bio,
+        courses: courses,
+      });
+    })
 };
 
 coursesCtrl.getSection = function (req, res) {
-  models.Section.findAll({
+  return models.Section.findAll({
     where: {
       CourseId: req.params.courseId,
     },
@@ -124,13 +136,16 @@ coursesCtrl.getNewSection = function (req, res) {
 };
 
 coursesCtrl.edit = function (req, res) {
-  models.Section.create(req.body)
-    .then(function (section) {
-      models.Course.findOne({where: {id: req.params.courseId}})
-        .then(function (course) {
-          course.addSection(section);
-          res.redirect('/' + req.user.username + '/' + req.params.courseId);
-        });
+  let section = null;
+
+  return models.Section.create(req.body)
+    .then(function (resSection) {
+      section = resSection;
+      return models.Course.findOne({where: {id: req.params.courseId}})
+    })
+    .then(function (course) {
+      course.addSection(section);
+      res.redirect('/' + req.user.username + '/' + req.params.courseId);
     });
 };
 
@@ -141,12 +156,15 @@ coursesCtrl.sectionNew = function (req, res) {
     content: req.body.content,
   }
 
-  models.Section.create(sectionData)
-    .then(function (section) {
-      models.Course.findOne({where: {id: req.params.courseId}})
-        .then(function (course) {
-          course.addSection(section);
-          res.redirect('/' + username + '/' + req.params.courseId);
-        });
+  let section = null;
+
+  return models.Section.create(sectionData)
+    .then(function (resSection) {
+      section = resSection;
+      return models.Course.findOne({where: {id: req.params.courseId}});
+    })
+    .then(function (course) {
+      course.addSection(section);
+      res.redirect('/' + username + '/' + req.params.courseId);
     });
 };
