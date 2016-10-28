6d499ea8e7f475418a4605aa1b8a3eae17b21d95
Theodore Keloglou <theodorekeloglou@gmail.com>
2016-10-28 21:59:11 +0300

    add slug name hook on course model

--- back/models/course.model.js
+++ back/models/course.model.js
@@ -1,10 +1,20 @@
+const slug = require('slug');
+
 module.exports = function (sequelize, DataTypes) {
   const attributes = {
     name: DataTypes.STRING,
+    slug: DataTypes.STRING,
     description: DataTypes.TEXT,
   };
 
-  var Course = sequelize.define('Course', attributes);
+  const Course = sequelize.define('Course', attributes);
+
+  const slugNameHook = function (course) {
+    course.slug = slug(course.name, { lower: true });
+  };
+
+  Course.beforeCreate(slugNameHook);
+  Course.beforeUpdate(slugNameHook);
 
   return Course;
 };
