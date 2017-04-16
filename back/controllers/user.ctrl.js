675745cf076d0604a0843243cddd11265a16be04
Theodore Keloglou <theodorekeloglou@gmail.com>
2017-04-16 20:10:08 +0300

    add user edit methods

--- back/controllers/user.ctrl.js
+++ back/controllers/user.ctrl.js
@@ -54,3 +54,53 @@ userCtrl.logout = function (req, res) {
   req.logout();
   res.redirect('/');
 };
+
+/**
+ * Render user edit view.
+ *
+ * @param {Object} req The express request object.
+ * @param {object} res The express response object.
+ */
+userCtrl.edit = function (req, res) {
+  models.User.findOne({ where: { id: req.user.id } })
+    .then(function (user) {
+      res.render('user-edit', {
+        username: user.username,
+        email: user.email,
+        name: user.name,
+        bio: user.bio,
+      });
+    });
+};
+
+/**
+ * Update user.
+ *
+ * @param {Object} req The express request object.
+ * @param {object} res The express response object.
+ */
+userCtrl.update = function (req, res) {
+  var updateData = {};
+  if (req.body.username) {
+    updateData.username = req.body.username;
+  }
+  if (req.body.email) {
+    updateData.email = req.body.email;
+  }
+  if (req.body.name) {
+    updateData.name = req.body.name;
+  }
+  if (req.body.bio) {
+    updateData.bio = req.body.bio;
+  }
+  if (req.body.password && req.body.password === req.body.passwordConfirmation) {
+    updateData.password = req.body.password;
+  }
+  models.User.findOne({username: req.user.username})
+    .then(function (user) {
+      return user.update(updateData);
+    })
+    .then(function () {
+      res.end();
+    });
+};
